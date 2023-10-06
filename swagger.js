b99f5256a227b3841a0d86549582e7f5649d35c5
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-10-06 02:01:49 -0400

    corrections

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API',
+    description: 'Api-Web-Services',
   },
   host: 'localhost:8080',
   schemes: ['http'],
