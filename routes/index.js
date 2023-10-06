a16b6c0cab5be8c93921a3b6e37aa69aa9522bdf
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-10-06 01:53:10 -0400

    api docs working in localhost and render

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,5 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts')); // app.METHOD(PATH/PATTERN, HANDLER) where PATH is a path on the server
 
-router.use(
-  '/',
-  (docData = (req, res) => {
-    let docData = {
-      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
-    };
-    res.send(docData);
-  })
-);
-
 
 module.exports = router;
